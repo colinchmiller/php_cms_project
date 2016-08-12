c650eb2ee5fdd1c8601c67311669e60cf0c6796f
colinmiller <colinchmiller@gmail.com>
2016-08-11 21:05:16 -0500

    add posts with image upload

--- admin/posts.php
+++ admin/posts.php
@@ -17,64 +17,31 @@
                           <small>Author</small>
                       </h1>
 
-                      <table class="table table-bordered table-hover">
-                        <thead>
-                          <tr>
-                            <th>ID</th>
-                            <th>Author</th>
-                            <th>Title</th>
-                            <th>Category</th>
-                            <th>Status</th>
-                            <th>Image</th>
-                            <th>Tags</th>
-                            <th>Comments</th>
-                            <th>Date</th>
-                          </tr>
-                        </thead>
-                        <tbody>
+                      <?php
 
-                          <?php
+                      if(isset($_GET['source'])){
+                        $source = $_GET['source'];
+                      } else {
+                        $source = '';
+                      }
 
-                          $query = "SELECT * FROM posts";
-                          $select_posts = mysqli_query($connection, $query);
+                      switch($source){
+                        case 'add_post';
+                        include "includes/add_post.php";
+                        break;
+                        echo "Forty Five";
+                        case '45';
+                        break;
+                        case '55';
+                        echo "Fifty Five";
+                        break;
 
-                          while($row = mysqli_fetch_assoc($select_posts)){
-                            $post_id = $row['post_id'];
-                            $post_author = $row['post_author'];
-                            $post_title = $row['post_title'];
-                            $post_category_id = $row['post_category_id'];
-                            $post_status = $row['post_status'];
-                            $post_image = $row['post_image'];
-                            $post_tags = $row['post_tags'];
-                            $post_comment_count = $row['post_comment_count'];
-                            $post_date = $row['post_date'];
+                        default:
+                        include "includes/view_all_posts.php";
+                        break;
+                      }
 
-                            echo "<tr>";
-                              echo "<td>{$post_id}</td>";
-                              echo "<td>{$post_author}</td>";
-                              echo "<td>{$post_title}</td>";
-                              echo "<td>{$post_category_id}</td>";
-                              echo "<td>{$post_status}</td>";
-                              echo "<td><img width='100' src='../images/{$post_image}' alt='image'</td>";
-                              echo "<td>{$post_tags}</td>";
-                              echo "<td>{$post_comment_count}</td>";
-                              echo "<td>{$post_date}</td>";
-                            echo "</tr>";
-                          }
-                           ?>
-
-                            <td>10</td>
-                            <td>Colin Miller</td>
-                            <td>Bootstrap Framework</td>
-                            <td>Bootstrap</td>
-                            <td>Status</td>
-                            <td>Image</td>
-                            <td>Tags</td>
-                            <td>Comments</td>
-                            <td>Date</td>
-
-                        </tbody>
-                      </table>
+                       ?>
 
 
                     </div>
