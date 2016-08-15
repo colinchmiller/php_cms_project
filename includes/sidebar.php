a653186030bc5b9250172f3e47d54cff4809e613
colinmiller <colinchmiller@gmail.com>
2016-08-15 09:36:07 -0500

    category links and truncated the post text

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -36,12 +36,13 @@
                   <?php
                   while($row = mysqli_fetch_assoc($select_categories_sidebar)){
                     $cat_title = $row['cat_title'];
-                    echo "<li><a href='#'>{$cat_title}</a></li>";
+                    $cat_id = $row['cat_id'];
+                    echo "<li><a href='category.php?category=$cat_id'>{$cat_title}</a></li>";
                   }
                   ?>
                 </ul>
             </div>
-            
+
         </div>
         <!-- /.row -->
     </div>
