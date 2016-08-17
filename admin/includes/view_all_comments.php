329f790aeb16116ee1fc8ac82492fa12aff0d1e2
colinmiller <colinchmiller@gmail.com>
2016-08-17 09:03:00 -0500

    minor edits to form organization so that header matches echoed content

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -46,11 +46,11 @@
 
         echo "<td>{$comment_email}</td>";
         echo "<td>{$comment_status}</td>";
+        echo "<td>Some Title</td>";
         echo "<td>{$comment_date}</td>";
-        echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Approve</a></td>";
-        echo "<td><a href='posts.php?delete={$post_id}'>Unapprove</a></td>";
-        echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
-        echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
+        echo "<td><a href='posts.php?source=edit_post&p_id={$comment_id}'>Approve</a></td>";
+        echo "<td><a href='posts.php?delete={$comment_id}'>Unapprove</a></td>";
+        echo "<td><a href='posts.php?delete={$comment_id}'>Delete</a></td>";
       echo "</tr>";
     }
      ?>
