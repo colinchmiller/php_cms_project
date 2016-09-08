0e45f03acf79dec79d505ce3867230b6fe572918
colinmiller <colinchmiller@gmail.com>
2016-09-08 14:51:25 -0500

    basic comment functionality

--- post.php
+++ post.php
@@ -60,14 +60,29 @@
 
               <!-- Blog Comments -->
 
+              <?php
+                if(isset($_POST['create_comment']))
+                echo $_POST['comment_author'];
+
+               ?>
+
               <!-- Comments Form -->
               <div class="well">
                   <h4>Leave a Comment:</h4>
-                  <form role="form">
-                      <div class="form-group">
-                          <textarea class="form-control" rows="3"></textarea>
-                      </div>
-                      <button type="submit" class="btn btn-primary">Submit</button>
+                  <form role="form" action="" method="post">
+                    <div class="form-group">
+                        <label for="Author">Author</label>
+                        <input type="text" name="comment_author" class="form-control" name="comment_author">
+                    </div>
+                    <div class="form-group">
+                      <label for="Email">Email</label>
+                        <input type="email" name="comment_email" class="form-control" name="comment_email">
+                    </div>
+                    <div class="form-group">
+                      <label for="Comment">Your Comment</label>
+                        <textarea name="comment_content" class="form-control" rows="3"></textarea>
+                    </div>
+                    <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
                   </form>
               </div>
 
