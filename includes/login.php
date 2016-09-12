be3f748d3e80bc3437448cd7462ea97d651891d9
colinmiller <colinchmiller@gmail.com>
2016-09-12 09:18:16 -0500

    add user session for information on login

--- includes/login.php
+++ includes/login.php
@@ -1,4 +1,5 @@
 <?php include "db.php"; ?>
+<?php session_start(); ?>
 
 <?php
 if(isset($_POST['login'])){
@@ -23,10 +24,14 @@ if(isset($_POST['login'])){
     $db_user_lastname = $row['user_lastname'];
     $db_user_role = $row['user_role'];
   }
-  //Validation
+  //validation on login form
   if($username !== $db_username && $password !== $db_user_password){
     header("Location: ../index.php");
   } else if ($username == $db_username && $password == $db_user_password){
+    $_SESSION['username'] = $db_username;
+    $_SESSION['firstname'] = $db_user_firstname;
+    $_SESSION['lastname'] = $db_user_lastname;
+    $_SESSION['user_role'] = $db_user_role;
     header("Location: ../admin");
   } else {
     header("Location: ../index.php");
