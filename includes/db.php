6ac09fa693967253eac6d6a81b2491ceee48449b
colinmiller <colinchmiller@gmail.com>
2016-09-19 12:35:07 -0500

    heroku db connection

--- includes/db.php
+++ includes/db.php
@@ -1,22 +1,41 @@
 <?php
 
 //put values of the connection in an array and make them constant and more secure
-$db['db_host'] = "localhost";
-$db['db_user'] = "root";
-$db['db_pass'] = "";
-$db['db_name'] = "CMS";
+//For Heroku and JawsDB
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
 
-foreach($db as $key => $value){
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
 
-  define(strtoupper($key), $value);
+//create connection
+$connection = new mysqli($hostname, $username, $password, $database);
 
+//check connection
+if($connection->connect_error){
+  die("Connection failed: " . $connection->connect_error);
 }
+echo "Connection was successfully established!";
 
-$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+// For Local Hosting
+// $db['db_host'] = "localhost";
+// $db['db_user'] = "root";
+// $db['db_pass'] = "";
+// $db['db_name'] = "CMS";
 
-if($connection){
-  //echo "We are connected to CMS";
-}
+// foreach($db as $key => $value){
+//
+//   define(strtoupper($key), $value);
+//
+// }
+//
+// $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+//
+// if($connection){
+//   //echo "We are connected to CMS";
+// }
 
 
 
